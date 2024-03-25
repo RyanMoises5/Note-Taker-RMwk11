ee4f2d50e91cd9b1580a4dc6d605f46443c4e449
Ryan M <ryan.a.moises@gmail.com>
2024-03-25 03:00:46 -0400

    Test

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -4,12 +4,43 @@ const util = require('util');
 
 const readFromFile = util.promisify(fs.readFile);
 
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+
 router.get('/', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 router.post('/', (req, res) => {
-    console.log("filler")
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        }
+        
+        readAndAppend(newNote, '.db/db.json');
+
+        const response = {
+            status: "success",
+            body: newNote
+        };
+
+        res.json(response);
+    
+    } else {
+        res.json('Error in posting note');
+    }
 });
 
 module.exports = router;
